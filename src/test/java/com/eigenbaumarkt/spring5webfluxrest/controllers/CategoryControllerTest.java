7ae4cfb13fe302f6d8a71c46f2d059ca50a52328
Mesqualito <jochen@gebsattel.rocks>
2020-10-20 17:20:56 +0200

    Setting up a CategoryControllerTest
    HINT:
    run with Test-Coverage in Intellij to see, what percentage of code is tested in the Unit-Test-Methods
    
    analog zu https://www.udemy.com/course/spring-framework-5-beginner-to-guru/learn/lecture/8911514, Abschnitt 26: "RESTFul Webservices with Spring Webflux" - Lektion 412

---
+++ src/test/java/com/eigenbaumarkt/spring5webfluxrest/controllers/CategoryControllerTest.java
@@ -0,0 +1,50 @@
+package com.eigenbaumarkt.spring5webfluxrest.controllers;
+
+import com.eigenbaumarkt.spring5webfluxrest.domain.Category;
+import com.eigenbaumarkt.spring5webfluxrest.repositories.CategoryRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+class CategoryControllerTest {
+
+    WebTestClient webTestClient;
+    CategoryRepository categoryRepository;
+    CategoryController categoryController;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        categoryRepository = Mockito.mock(CategoryRepository.class);
+        categoryController = new CategoryController(categoryRepository);
+        webTestClient = WebTestClient.bindToController(categoryController).build();
+    }
+
+    @Test
+    void listAllCategories() {
+        // Behaviour-Driven Mockito
+        BDDMockito.given(categoryRepository.findAll())
+                .willReturn(Flux.just(Category.builder().description("Test-Category 1").build(),
+                        Category.builder().description("Test-Category 2").build()));
+
+        webTestClient.get()
+                .uri("/api/v1/categories")
+                .exchange()
+                .expectBodyList(Category.class)
+                .hasSize(2);
+    }
+
+    @Test
+    void listCategoryById() {
+        BDDMockito.given(categoryRepository.findById("someId"))
+                .willReturn(Mono.just(Category.builder().description("Test-Category").build()));
+
+        webTestClient.get()
+                .uri("/api/v1/categories/someid")
+                .exchange()
+                .expectBody(Category.class);
+    }
+}
